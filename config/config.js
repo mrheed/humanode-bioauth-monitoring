e4c9b4f80c76546735ff34cd38b993d019fe2ca9
mrgb <porrosyhd123@MRGB-P725>
2024-05-19 16:14:04 +0700

    feat: getting ws tunnel url

--- config/config.js
+++ config/config.js
@@ -5,5 +5,5 @@ export const config = {
   bioauthStatusUrl: process.env.BIO_AUTH_URL || 'http://127.0.0.1:9933', // Default to local URL if environment variable is not set,
   validatorName: process.env.VALIDATOR_NAME || '',
   bioAuthVerificationUrl: process.env.BIO_AUTH_VERIFICATION_URL || '',
-  humanodeDir: process.env.HUMANODE_DIR || ''
+  humanodeDir: (process.env.HUMANODE_DIR || '').replace('$HOME', process.env.HOME)
 }
